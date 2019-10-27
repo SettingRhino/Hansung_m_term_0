3804ac6522bf627f303b5d5d0015c9a2d52f24c8
SettingRhino <tjtj54441@gmail.com>
2019-10-28 05:32:36 +0900

    base_1
    add board_0(sample of board) activity
    add setlistener boardcategory
    add drawble->textviewbottomsolid
    Applied background color

---
+++ Board_0.java
@@ -0,0 +1,14 @@
+package seven.hansung.nonamed;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Board_0 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board_0);
+
+    }
+}
